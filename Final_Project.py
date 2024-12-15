c4d9db4265cacb67976846be414139a18069d032
Bukola Famurewa <Famurewaoluwabukolamartha@gmail.com>
2024-12-14 20:07:11 -0500

    Added the NYT API and it finally works

--- Final_Project.py
+++ Final_Project.py
@@ -2,39 +2,94 @@ import requests
 import os        
 from dotenv import load_dotenv  
 
-# Loading environment variables from the .env file
+# Loading API keys from the .env file
 load_dotenv()
 
-# Retrieving the API key from the environment variables
-api_key = os.getenv("TMDB_API_KEY") 
+class MovieSearch:
+    def __init__(self, tmdb_api_key, nyt_api_key):
+        # Initializing the class with the API keys
+        self.tmdb_api_key = tmdb_api_key
+        self.nyt_api_key = nyt_api_key
+        self.tmdb_base_url = "https://api.themoviedb.org/3/"
+        self.nyt_base_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
 
-# Defining the base URL for TMDB API
-base_url = "https://api.themoviedb.org/3/"
+    def search_movie(self, movie_name):
+        """Search for a movie using the TMDB API"""
+        # Creating the URL to search for the movie
+        search_url = f"{self.tmdb_base_url}search/movie"
+        
+        # Parameters to send in the API request
+        params = {
+            "api_key": self.tmdb_api_key,  #  TMDB API key
+            "query": movie_name  
+        }
 
-# Searching endpoint to search for movies
-search_url = f"{base_url}search/movie"
+        # Sending a GET request to TMDB API
+        response = requests.get(search_url, params=params)
 
-# Setting the parameters for the API request
-params = {
-    "api_key": api_key,  
-    "query": "Inception"  
-}
+        if response.status_code == 200:
+            movie_data = response.json()
 
-# Making teh API request using GET method
-response = requests.get(search_url, params=params)
+            # Checking if the movie was found
+            if movie_data['results']:
+                print(f"Title: {movie_data['results'][0]['title']}")
+                print(f"Release Date: {movie_data['results'][0]['release_date']}")
+                print(f"Overview: {movie_data['results'][0]['overview']}")
+                return movie_data['results'][0]['title'] 
+            else:
+                print("No movies found.")
+                return None
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+            return None
 
-# Checking if the request was successful
-if response.status_code == 200:
-    # Converting the JSON response to a Python dictionary
-    movie_data = response.json()
+    def search_movie_reviews(self, movie_name):
+        """Search for movie reviews using the NYT Article Search API"""
+        # Parameters to send in the API request to search for movie reviews
+        params = {
+            "q": movie_name, 
+            "fq": 'section_name:"Movies" AND type_of_material:"Review"',  
+            "api-key": self.nyt_api_key,  
+            "page": 0  
+        }
 
-    # Checking to see if works
-    if movie_data['results']:
-        print(f"Title: {movie_data['results'][0]['title']}")
-        print(f"Release Date: {movie_data['results'][0]['release_date']}")
-        print(f"Overview: {movie_data['results'][0]['overview']}")
-    else:
-        print("No movies found.")
-else:
-    # Printing the error status, If teh code doesn't work
-    print(f"Error: {response.status_code} - {response.text}")
+        # Sending a GET request to NYT API
+        response = requests.get(self.nyt_base_url, params=params)
+
+        if response.status_code == 200:
+            # If the request was successful
+            review_data = response.json()
+
+            # Checking if reviews are available
+            if review_data['response']['docs']:
+                print("\nMovie Reviews:")
+                # Loop through each article (review)
+                for article in review_data['response']['docs']:
+                    print(f"Title: {article['headline']['main']}")
+                    print(f"Snippet: {article['snippet']}")
+                    print(f"URL: {article['web_url']}")
+                    print("-------------")
+            else:
+                print("No reviews found.")
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+
+class MovieReview:
+    def __init__(self, tmdb_api_key, nyt_api_key):
+        self.movie_search = MovieSearch(tmdb_api_key, nyt_api_key)
+
+    def run(self, movie_name):
+        movie_title = self.movie_search.search_movie(movie_name)
+
+        # If the movie is found, search for reviews
+        if movie_title:
+            self.movie_search.search_movie_reviews(movie_title)
+
+if __name__ == "__main__":
+    # Retrieving the API keys from environment variables
+    tmdb_api_key = os.getenv("TMDB_API_KEY") 
+    nyt_api_key = os.getenv("NYT_API_KEY")
+
+    # Creating an instance of the app and running it
+    app = MovieReview(tmdb_api_key, nyt_api_key)  # Corrected here
+    app.run("Inception")  
